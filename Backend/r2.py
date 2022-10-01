815415ec59ab1379ff19726aa7845f3ac1540503
Annie Liang <aaliang@bu.edu>
2022-10-01 13:49:33 -0400

    did this do anything

---
+++ Backend/r2.py
@@ -0,0 +1,22 @@
+import requests
+
+# Get categories
+response = requests.get("https://api.cs50.io/dining/recipes")
+
+# Convert JSON to list of dicts
+recipes = response.json()
+
+CaloriesList = [] 
+rows, cols =  len(recipes), 2
+
+# Print each recipe's name
+for recipe in recipes:
+    calorieslist = {'Name': recipe["name"], 'Calories': recipe["calories"]}
+    print(recipe["name"])
+    print("calories: ", recipe["calories"])
+    
+    CaloriesList += [[recipe["name"], recipe["calories"]]]
+
+print(CaloriesList)
+
+
