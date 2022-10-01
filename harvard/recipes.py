f85978c7bf35661a3ddaabcbb21ee1ccaf942f35
Annie Liang <aaliang@bu.edu>
2022-10-01 13:25:21 -0400

    idk

--- harvard/recipes.py
+++ harvard/recipes.py
@@ -6,8 +6,21 @@ response = requests.get("https://api.cs50.io/dining/recipes")
 # Convert JSON to list of dicts
 recipes = response.json()
 
+CaloriesList = []
+rows, cols =  len(recipes),len(recipes)
+
+CaloriesList = [[cols]rows]
+
 # Print each recipe's name
 for recipe in recipes:
+    calorieslist = {'Name': recipe["name"], 'Calories': recipe["calories"]}
     print(recipe["name"])
     print("calories: ", recipe["calories"])
-    print()
+    
+    
+
+    CaloriesList += [calorieslist["Name"], calorieslist["Calories"]]
+
+print()
+print(CaloriesList)
+
